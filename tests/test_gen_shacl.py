fb6e4522ab0ad21c9291f4e15be3042e8cb09ffc
Alban Gaignard <alban.gaignard@univ-nantes.fr>
2021-03-19 17:27:20 +0100

    test SHACL validation

--- tests/test_gen_shacl.py
+++ tests/test_gen_shacl.py
@@ -4,114 +4,38 @@ from jinja2 import Template
 from pyshacl import validate
 
 from metrics.R2Impl import R2Impl
+from profiles.bioschemas_shape_gen import  gen_SHACL_from_profile
+from profiles.bioschemas_shape_gen import  validate_shape_from_RDF
+from profiles.bioschemas_shape_gen import  validate_shape_from_microdata
 
 class GenSHACLTestCase(unittest.TestCase):
 
-    """to match an RDF resource, use sh:targetClass or sh:targetSubjectOf"""
+    def test_validate_shape_tool(self):
 
-    accessibility_properties = ['schema:licenses']
+        #classes = ['schema:Dataset']
+        #minimal_dataset_properties = ['schema:name', 'schema:description', 'schema:identifier', 'schema:keywords', 'schema:url']
+        #recommended_dataset_properties = ['schema:license', 'schema:creator', 'schema:citation']
 
-    shape_template = """
-        @prefix dash: <http://datashapes.org/dash#> .
-        @prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .
-        @prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .
-        @prefix schema: <http://schema.org/> .
-        @prefix sh: <http://www.w3.org/ns/shacl#> .
-        @prefix xsd: <http://www.w3.org/2001/XMLSchema#> .
-        @prefix edam: <http://edamontology.org/> .
-        @prefix biotools: <https://bio.tools/ontology/> .
+        classes = ['schema:SoftwareApplication']
+        minimal_software_properties = ['schema:name', 'schema:description', 'schema:url']
+        recommended_software_properties = ['schema:additionalType', 'schema:applicationCategory',
+                                           'schema:applicationSubCategory', 'schema:author' 'schema:license',
+                                           'schema:citation', 'schema:featureList', 'schema:softwareVersion']
 
-        schema:AccessibilityShape
-            a sh:NodeShape ;
-            #sh:targetSubjectsOf schema:name ;
-            sh:targetClass schema:ComputationalWorkflow, schema:SoftwareApplication ;
+        shape = gen_SHACL_from_profile("toolShape", target_classes=classes, min_props=minimal_software_properties, rec_props=recommended_software_properties)
 
-            {% for prop_name in data['properties'] %}
-            sh:property [
-                sh:path {{prop_name}} ;
-                sh:minCount 1 ;
-                sh:severity sh:Violation
-            ] ;
-            {% endfor %}
-        .
-    """
+        validate_shape_from_RDF(input_uri='https://bio.tools/api/jaspar?format=jsonld', rdf_syntax='json-ld', shacl_shape=shape)
 
-    def gen_SHACL_from_prop(self, property_list=None):
-        r2_1 = R2Impl()
-        r2_1.set_url("https://workflowhub.eu/workflows/45")
-        r2_1.extract_html_requests()
-        r2_1.extract_rdf()
-        kg1 = r2_1.get_jsonld()
+    def test_validate_shape_dataset(self):
 
-        #r2_2 = R2Impl()
-        #r2_2.set_url("https://bio.tools/jaspar")
-        #r2_2.extract_html_requests()
-        #r2_2.extract_html_selenium()
-        #r2_2.extract_rdf()
-        #kg2 = r2_2.get_jsonld()
+        classes = ['schema:Dataset']
+        minimal_dataset_properties = ['schema:name', 'schema:description', 'schema:identifier', 'schema:keywords', 'schema:url']
+        recommended_dataset_properties = ['schema:license', 'schema:creator', 'schema:citation']
 
-        kg2 = ConjunctiveGraph()
-        kg2.parse(location='https://bio.tools/api/jaspar?format=jsonld',format='json-ld')
+        shape = gen_SHACL_from_profile("datasetShape", target_classes=classes, min_props=minimal_dataset_properties, rec_props=recommended_dataset_properties)
 
-        kg = kg1 + kg2
+        validate_shape_from_microdata(input_uri='https://data.inrae.fr/dataset.xhtml?persistentId=doi:10.15454/PL3HWQ', shacl_shape=shape)
 
-        print(kg.serialize(format="turtle").decode())
-        # print(len(kg1))
-        #print(len(kg2))
-        #print(kg2.serialize(format="turtle").decode())
-        #print(len(kg))
-
-        data = {
-            'properties': self.accessibility_properties,
-        }
-
-        template = Template(self.shape_template)
-        shape = template.render(data=data)
-        print(shape)
-        g = ConjunctiveGraph()
-        g.parse(data=shape, format='turtle')
-        print(len(g))
-
-        r = validate(data_graph=kg,
-                     data_graph_format='turtle',
-                     shacl_graph=shape,
-                     # shacl_graph = my_shacl_constraint,
-                     shacl_graph_format='turtle',
-                     ont_graph=None,
-                     inference='rdfs',
-                     abort_on_error=False,
-                     meta_shacl=False,
-                     debug=True)
-
-        conforms, results_graph, results_text = r
-
-        report_query = """
-                SELECT ?node ?path ?severity WHERE {
-                    ?v rdf:type sh:ValidationReport ;
-                       sh:result ?r .
-                    ?r sh:focusNode ?node ;
-                       sh:sourceShape ?s .
-                    ?s sh:path ?path ;
-                       sh:severity ?severity .
-                }
-            """
-
-        results = results_graph.query(report_query)
-        #print('VALIDATION RESULTS')
-        #print(results_text)
-        #print(conforms)
-        #print(results_graph.serialize(format="turtle").decode())
-        warnings = []
-        errors = []
-        for r in results:
-            if "#Warning" in r['severity']:
-                print(f'WARNING = Property {r["path"]} should be provided for {r["node"]}')
-            if "#Violation" in r['severity']:
-                print(f'ERROR = Property {r["path"]} must be provided for {r["node"]}')
-
-
-    def test_something(self):
-        self.gen_SHACL_from_prop(self)
 
 
 
