bd821c27537cc8171da776e9358842d62987f9ac
Alban Gaignard <alban.gaignard@univ-nantes.fr>
2022-01-25 10:38:03 +0100

    removed out of scope test

--- tests/test_custom_metrics.py
+++ tests/test_custom_metrics.py
@@ -68,17 +68,5 @@ class OlsLovTestCase(unittest.TestCase):
         print(res)
 
 
-class RandomTest(unittest.TestCase):
-
-    """Test case utilisé pour tester les fonctions du module 'random'."""
-
-    def test_choice(self):
-        """Test le fonctionnement de la fonction 'random.choice'."""
-        liste = list(range(10))
-        elt = random.choice(liste)
-        # Vérifie que 'elt' est dans 'liste'
-        self.assertIn(elt, liste)
-
-
 if __name__ == "__main__":
     unittest.main()
