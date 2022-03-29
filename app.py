32d161c1246b591dc8888f13ec24d51ddbcce2eb
Alban Gaignard <alban.gaignard@univ-nantes.fr>
2022-03-29 15:26:43 +0200

    fixed defult IP

--- app.py
+++ app.py
@@ -1448,7 +1448,7 @@ if __name__ == "__main__":
     elif args.web:
         logging.info("Starting webserver")
         try:
-            socketio.run(app, host="127.0.0.1", port=5000, debug=True)
+            socketio.run(app, host="0.0.0.0", port=5000, debug=True)
         finally:
             browser = WebResource.WEB_BROWSER_HEADLESS
             browser.quit()
