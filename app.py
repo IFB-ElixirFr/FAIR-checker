89e59710a94920b265f917e96fe26247396d6361
Thomas Rosnet <thomas.rosnet@gmail.com>
2022-07-29 10:37:47 +0200

    prototype working of FC API

--- app.py
+++ app.py
@@ -15,7 +15,10 @@ from flask import (
     send_file,
     send_from_directory,
     make_response,
+    Blueprint
 )
+from flask_restx import Resource, Api
+from flask_swagger_ui import get_swaggerui_blueprint
 from flask_cors import CORS
 from flask_socketio import SocketIO
 from flask_socketio import emit
@@ -53,6 +56,32 @@ from profiles.bioschemas_shape_gen import validate_any_from_microdata
 import git
 
 app = Flask(__name__)
+
+app.config.SWAGGER_UI_OPERATION_ID = True
+app.config.SWAGGER_UI_REQUEST_DURATION = True
+
+@app.route('/')
+def index():
+    return render_template(
+        "index.html",
+        title="FAIR-Checker",
+        subtitle="Improve the FAIRness of your web resources",
+    )
+
+# blueprint = Blueprint('api', __name__, url_prefix='/api')
+
+api = Api(app,
+    title='FAIR-Checker API',
+    doc='/swagger',
+    base_path='/api',
+    # base_url='/'
+    )
+
+# app.register_blueprint(blueprint)
+
+metrics_namespace = api.namespace('metrics', description='Metrics assessment')
+fc_api_namespace = api.namespace('api', description='FAIR Metrics assessment')
+
 
 app.logger.setLevel(logging.DEBUG)
 CORS(app)
@@ -108,31 +137,7 @@ metrics = [
 METRICS = {}
 # json_metrics = test_metric.getMetrics()
 factory = FAIRMetricsFactory()
-#
-# # for i in range(1,3):
-# try:
-#     # metrics.append(factory.get_metric("test_f1"))
-#     # metrics.append(factory.get_metric("test_r2"))
-#     for metric in json_metrics:
-#         # remove "FAIR Metrics Gen2" from metric name
-#         name = metric["name"].replace("FAIR Metrics Gen2- ", "")
-#         # same but other syntax because of typo
-#         name = name.replace("FAIR Metrics Gen2 - ", "")
-#         principle = metric["principle"]
-#         METRICS[name] = factory.get_metric(
-#             name,
-#             metric["@id"],
-#             metric["description"],
-#             metric["smarturl"],
-#             principle,
-#             metric["creator"],
-#             metric["created_at"],
-#             metric["updated_at"],
-#         )
 
-# except ValueError as e:
-#     print(f"no metrics implemention for {e}")
-#
 # # A DEPLACER AU LANCEMENT DU SERVEUR ######
 # METRICS_RES = test_metric.getMetrics()
 
@@ -148,6 +153,27 @@ RDF_TYPE = {}
 FILE_UUID = ""
 
 DICT_TEMP_RES = {}
+
+# SWAGGER_URL = '/api/docs'  # URL for exposing Swagger UI (without trailing '/')
+# API_URL = 'http://petstore.swagger.io/v2/swagger.json'  # Our API url (can of course be a local resource)
+# # Call factory function to create our blueprint
+# swaggerui_blueprint = get_swaggerui_blueprint(
+#     SWAGGER_URL,  # Swagger UI static files will be mapped to '{SWAGGER_URL}/dist/'
+#     API_URL,
+#     config={  # Swagger UI config overrides
+#         'app_name': "Test application"
+#     },
+#     # oauth_config={  # OAuth config. See https://github.com/swagger-api/swagger-ui#oauth2-configuration .
+#     #    'clientId': "your-client-id",
+#     #    'clientSecret': "your-client-secret-if-required",
+#     #    'realm': "your-realms",
+#     #    'appName': "your-app-name",
+#     #    'scopeSeparator': " ",
+#     #    'additionalQueryStringParams': {'test': "hello"}
+#     # }
+# )
+#
+# app.register_blueprint(swaggerui_blueprint)
 
 
 @app.context_processor
@@ -172,19 +198,27 @@ def favicon():
     )
 
 
+# @app.route("/")
+# def home():
+#     return render_template(
+#         "index.html",
+#         title="FAIR-Checker",
+#         subtitle="Improve the FAIRness of your web resources",
+#     )
+
+# @api.route('/')
+# class FairChecker(Resource):
+#     def get():
+#         return redirect(url_for('home'), code=302)
+
+
+
 @app.route("/")
 def home():
     return render_template(
-        "index.html",
-        title="FAIR-Checker",
-        subtitle="Improve the FAIRness of your web resources",
-    )
-
-
-@app.route("/")
-def index():
-    return render_template(
-        "index.html",
+        # "index.html",
+        # title="FAIR-Checker",
+        # subtitle="Improve the FAIRness of your web resources",
     )
 
 
@@ -225,6 +259,65 @@ def statistics():
         r_success=stats.this_week_for_named_metrics(prefix="R", success=1),
         r_failures=stats.this_week_for_named_metrics(prefix="R", success=0),
     )
+
+@fc_api_namespace.route('/square/<int:num>')
+class TestSquare(Resource):
+    def get(self, num):
+        return {'data': num ** 2}
+
+@fc_api_namespace.route('/metric_F1A/<path:url>')
+class MetricEvalF1A(Resource):
+    def get(self, url):
+        web_res = WebResource(url)
+        metric = FAIRMetricsFactory.get_F1A(web_res)
+        result = metric.evaluate()
+
+        data = {
+            'score': result.get_score(),
+            'target_uri': result.get_target_uri(),
+            'metric': result.get_metrics(),
+            'eval_time': str(result.get_test_time()),
+            'recommendation': result.get_recommendation(),
+            'comment': result.get_log(),
+        }
+        return data
+
+@fc_api_namespace.route('/metrics_all/<path:url>')
+class MetricEvalAll(Resource):
+    def get(self, url):
+        web_res = WebResource(url)
+
+        metrics_collection = []
+        metrics_collection.append(FAIRMetricsFactory.get_F1A(web_res))
+        metrics_collection.append(FAIRMetricsFactory.get_F1B(web_res))
+        metrics_collection.append(FAIRMetricsFactory.get_F2A(web_res))
+        metrics_collection.append(FAIRMetricsFactory.get_F2B(web_res))
+        metrics_collection.append(FAIRMetricsFactory.get_I1(web_res))
+        metrics_collection.append(FAIRMetricsFactory.get_I1A(web_res))
+        metrics_collection.append(FAIRMetricsFactory.get_I1B(web_res))
+        metrics_collection.append(FAIRMetricsFactory.get_I2(web_res))
+        metrics_collection.append(FAIRMetricsFactory.get_I2A(web_res))
+        metrics_collection.append(FAIRMetricsFactory.get_I2B(web_res))
+        metrics_collection.append(FAIRMetricsFactory.get_I3(web_res))
+        metrics_collection.append(FAIRMetricsFactory.get_R11(web_res))
+        metrics_collection.append(FAIRMetricsFactory.get_R12(web_res))
+        metrics_collection.append(FAIRMetricsFactory.get_R13(web_res))
+
+        results = []
+        for metric in metrics_collection:
+            result = metric.evaluate()
+            data = {
+                'metric': result.get_metrics(),
+                'score': result.get_score(),
+                'target_uri': result.get_target_uri(),
+                'eval_time': str(result.get_test_time()),
+                'recommendation': result.get_recommendation(),
+                'comment': result.get_log(),
+            }
+            results.append(data)
+
+        return results
+
 
 
 @socketio.on("webresource")
