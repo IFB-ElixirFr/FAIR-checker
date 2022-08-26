93bbe9978032e7bf03183a001815672bc1ecc970
Thomas Rosnet <thomas.rosnet@gmail.com>
2022-08-26 17:22:10 +0200

    black

--- docs/conf.py
+++ docs/conf.py
@@ -19,9 +19,9 @@ import git
 
 # -- Project information -----------------------------------------------------
 
-project = 'FAIR-Checker'
-copyright = '2022, Thomas Rosnet, Alban Gaignard, Marie-Dominique Devignes'
-author = 'Thomas Rosnet, Alban Gaignard, Marie-Dominique Devignes'
+project = "FAIR-Checker"
+copyright = "2022, Thomas Rosnet, Alban Gaignard, Marie-Dominique Devignes"
+author = "Thomas Rosnet, Alban Gaignard, Marie-Dominique Devignes"
 
 # The full version, including alpha/beta/rc tags
 repo = git.Repo("..")
@@ -36,16 +36,16 @@ release = str(latest_tag)
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-	'sphinx_rtd_theme',
+    "sphinx_rtd_theme",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -53,10 +53,10 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-#html_theme = 'alabaster'
+# html_theme = 'alabaster'
 html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
