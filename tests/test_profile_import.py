ad28418562da640ca6676e09fbe75fa5efa834e6
Thomas Rosnet <thomas.rosnet@gmail.com>
2022-11-22 16:01:07 +0100

    updated profile retreive test

--- tests/test_profile_import.py
+++ tests/test_profile_import.py
@@ -46,11 +46,13 @@ class ImportBSProfileTestCase(unittest.TestCase):
     def test_github_rate_limite(self):
         url = "https://api.github.com/rate_limit"
         response = requests.get(url, headers=self.headers)
-        print(response.json())
+        print("Remaining: " + str(response.json()["resources"]["core"]["remaining"]))
+
 
 
     @unittest.skip("Need github TOKEN key to work")
     def test_import_bs_specs(self):
+        self.test_github_rate_limite()
         url = "https://api.github.com/repos/BioSchemas/specifications/contents"
         # with self.test_app.app_context():
         #     print(current_app.config)
@@ -101,49 +103,66 @@ class ImportBSProfileTestCase(unittest.TestCase):
                                     # print(m.group(2))
                                     
                                     if "RELEASE" in res["download_url"]:
-                                        releases[res["download_url"]] = m.group(1)
+                                        releases[res["download_url"]] = float(m.group(1))
                                         # releases[m.group(1)] = res["download_url"]
                                     elif "DRAFT" in res["download_url"]:
-                                        drafts[res["download_url"]] = m.group(1)
+                                        drafts[res["download_url"]] = float(m.group(1))
 
-                                    response = requests.get(res["download_url"], headers=self.headers)
-                                    jsonld = response.json()
-                                    profile_dict = {
-                                        "name": profile_name,
-                                        "file": res["download_url"],
-                                        "required": [],
-                                        "recommended": [],
-                                        "optional": [],
-                                    }
 
-                                    if "required" in jsonld["@graph"][0]["$validation"]:
-                                        profile_dict["required"] = jsonld["@graph"][0]["$validation"]["required"]
-                                    if "recommended" in jsonld["@graph"][0]["$validation"]:
-                                        profile_dict["recommended"] = jsonld["@graph"][0]["$validation"]["recommended"]
-                                    if "optional" in jsonld["@graph"][0]["$validation"]:
-                                        profile_dict["optional"] = jsonld["@graph"][0]["$validation"]["optional"]
-
-                                    profiles_list.append(profile_dict)
 
                             
-
+                            url_profile = ""
                             if releases:
                                 # print(releases.keys())
+                                print("Releases: ")
                                 print(releases.values())
+                                if releases.values():
+                                    latest_rel = max(releases.values())
+                                    print(latest_rel)
+                                    print("Release Key value: ", list(releases.keys())[list(releases.values()).index(latest_rel)])
+                                    print("Release Key value: ", [k for k, v in releases.items() if v == latest_rel])
+                                url_profile = list(releases.keys())[list(releases.values()).index(latest_rel)]
                                 # sort_orders = sorted(releases.items(), key=lambda x: x[1], reverse=True)
                                 # sort_orders.values()
                             else:
+                                print("Drafts: ")
                                 # print(drafts.keys())
                                 print(drafts.values())
+                                if drafts.values():
+                                    latest_rel = max(drafts.values())
+                                    print(latest_rel)
+                                    print("Draft Key value: ", list(drafts.keys())[list(drafts.values()).index(latest_rel)])
+                                    print("Draft Key value: ", [k for k, v in drafts.items() if v == latest_rel])
+                                    url_profile = list(drafts.keys())[list(drafts.values()).index(latest_rel)]
                                 # sort_orders = sorted(drafts.items(), key=lambda x: x[1], reverse=True)
                                 # sort_orders.values()
+
+                            if url_profile:
+                                response = requests.get(url_profile, headers=self.headers)
+                                jsonld = response.json()
+                                profile_dict = {
+                                    "name": profile_name,
+                                    "file": url_profile,
+                                    "required": [],
+                                    "recommended": [],
+                                    "optional": [],
+                                }
+
+                                if "required" in jsonld["@graph"][0]["$validation"]:
+                                    profile_dict["required"] = jsonld["@graph"][0]["$validation"]["required"]
+                                if "recommended" in jsonld["@graph"][0]["$validation"]:
+                                    profile_dict["recommended"] = jsonld["@graph"][0]["$validation"]["recommended"]
+                                if "optional" in jsonld["@graph"][0]["$validation"]:
+                                    profile_dict["optional"] = jsonld["@graph"][0]["$validation"]["optional"]
+
+                                profiles_list.append(profile_dict)
 
 
 
 
                 # if i > 20:
                 #     break
-            # print(profiles_list)
+            print(profiles_list)
 
         else:
             print('Content was not found.')
