9ad76150f1dfa76c6160450fc4b8f2b5a6d8f8d9
Thomas Rosnet <thomas.rosnet@gmail.com>
2023-03-10 15:42:51 +0100

    adding the evaluation cleaning/docs

--- metrics/Evaluation.py
+++ metrics/Evaluation.py
@@ -21,6 +21,24 @@ class Result(Enum):
 
 #########################
 class Evaluation:
+    """
+    The classe that represent an evaluation, can usef a WebResource and store all de evaluations results and metadata.
+
+    Attributes:
+        eval_logger (logging): The logger used to manage the logs of the evaluation
+        log_capture_string (StringIO): The variable containing the log stream
+        start_time (datetime): Store the starttime of the evaluation
+        end_time (datetime): Store the endtime of the evaluation
+        score (str): The score of the evaluation, can be 0, 1 or 2
+        recommendation (str): The recommendation for the user
+        result_text (_type_): _description_
+        reason (_type_): _description_
+        web_resource (WebResource): The evaluated WebResource object
+        metrics (str): The tag of the metric used to evaluate the WebResource
+        target_uri (str): The URI/URL of the evaluated resource
+        implem (str): The name of the implementor of the metric
+
+    """
     eval_logger = None
     log_capture_string = None
 
@@ -30,6 +48,7 @@ class Evaluation:
     recommendation = "No recommendation, metric validated"
 
     # Result_tet and reason are used by FAIRMetrics only (result_text: whole nanopub content, reason: comment/log)
+    # Might be deprecated
     result_text = None
     reason = None
 
@@ -38,17 +57,15 @@ class Evaluation:
     target_uri = None
     implem = None
 
-    # def __init__(self):
-    #     pass
-
     def __init__(self):
+        """
+        The constructor, instantiate the logger that stores the executions logs in a variable that can be passed to the UI
+        """
         self.log_capture_string = StringIO()
         ### Create the logger
-        # self.eval_logger = logging.getLogger("eval_logger")
         logger_id = str(uuid.uuid4())
         self.eval_logger = logging.getLogger(logger_id)
         self.eval_logger.setLevel(logging.INFO)
-        # self.eval_logger.propagate = False
 
         ### Setup the console handler with a StringIO object
         console_handler = logging.StreamHandler(self.log_capture_string)
@@ -57,8 +74,6 @@ class Evaluation:
         ### Add a formatter
         formatter = logging.Formatter(
             "%(levelname)s - %(message)s",
-            # "%Y-%m-%d %H:%M:%S"
-            # "[%(asctime)s] - %(levelname)s - %(message)s", "%Y-%m-%d %H:%M:%S"
         )
         console_handler.setFormatter(formatter)
 
@@ -67,90 +82,237 @@ class Evaluation:
         self.eval_logger.propagate = False
 
     def log_debug(self, message):
+        """
+        Add a debug message to the eval_logger
+
+        Args:
+            message (str): The text to add to the logger as debug
+        """
         self.eval_logger.debug(message)
 
     def log_info(self, message):
+        """
+        Add an info message to the eval_logger
+
+        Args:
+            message (str): The text to add to the logger as info
+        """
         self.eval_logger.info(message)
 
     def log_warning(self, message):
+        """
+        Add a warning message to the eval_logger
+
+        Args:
+            message (str): The text to add to the logger as warning
+        """
         self.eval_logger.warning(message)
 
     def log_error(self, message):
+        """
+        Add an error message to the eval_logger
+
+        Args:
+            message (str): The text to add to the logger as error
+        """
         self.eval_logger.error(message)
 
     def log_critical(self, message):
+        """
+        Add a critical message to the eval_logger
+
+        Args:
+            message (str): The text to add to the logger as critical
+        """
         self.eval_logger.critical(message)
 
-    def get_log(self):
-        return self.log_capture_string.getvalue()
-
-    def get_log_html(self):
-        return self.log_capture_string.getvalue().replace("\n", "<br>")
-
     def close_log_stream(self):
+        """
+        Close the string stream of the logger
+        """
         self.log_capture_string.close()
 
     def set_score(self, score):
+        """
+        Set the resulting score of the evaluation
+
+        Args:
+            score (str): A number representif the evaluation score. Usually 0 (failed), 1 (weak), 2 (strong)
+        """
         self.score = str(int(float(score)))
 
     def set_start_time(self):
+        """
+        Set the start time of the evaluation
+        """
         self.start_time = self.get_current_time()
 
     def set_end_time(self):
+        """
+        Set the end time of the evaluation
+        """
         self.end_time = self.get_current_time()
 
     def set_recommendations(self, recommendation_text):
+        """
+        Set the recommendations depending on the evaluation result to help the user improve its score
+
+        Args:
+            recommendation_text (str): The text of the recommendation
+        """
         self.recommendation = recommendation_text
 
-    # used by FAIRMetrics, will probably be replaced by logs
+    # used by FAIRMetrics, need to be replaced by logs
     def set_reason(self, r):
         self.reason = r
 
-    # used by FAIRMetrics, will probably be replaced by logs
+    # used by FAIRMetrics, need to be replaced by logs
     def append_reason(self, r):
         self.reason = self.reason + "\n" + r
 
     def set_web_resource(self, web_resource):
+        """
+        Set the web resource instance, contains the RDF metadata collected from the resource URL
+
+        Args:
+            web_resource (WebResource): The WebResource object instance that will be used for the evaluation
+        """
         self.web_resource = web_resource
 
     def set_metrics(self, metrics):
+        """
+        Set the TAG of the metric that will be used to evaluate the WebResource
+
+        Args:
+            metrics (str): The TAG of the metruc use to evaluate the WebResource
+        """
         self.metrics = str(metrics)
 
     def set_target_uri(self, target_uri):
+        """
+        Set the URI that is behind evaluated, available in the WebResource object
+
+        Args:
+            target_uri (str): The resource URI/URL
+        """
         self.target_uri = str(target_uri)
 
     def set_implem(self, implem):
+        """
+        Set the implementator of the metric, at the moment it is FAIR-Checker, or FAIRMetrics
+
+        Args:
+            implem (str): The name of the implementator of the metric
+        """
         self.implem = implem
+
+    def get_log(self):
+        """
+        Get all the string content stored in the logger
+
+        Returns:
+            str: The full log of the evaluation
+        """
+        return self.log_capture_string.getvalue()
+
+    def get_log_html(self):
+        """
+        Get all the string content stored in the logger and making it html compatible
+
+        Returns:
+            str: The full log of the evaluation
+        """
+        return self.log_capture_string.getvalue().replace("\n", "<br>")
 
     # TODO check https://pymongo.readthedocs.io/en/stable/examples/datetimes.html
     def get_current_time(self):
+        """
+        Get the current time
+
+        Returns:
+            datetime: Datetime object
+        """
         # return datetime.strptime(time.strftime('%Y-%m-%d %H:%M:%S'), '%Y-%m-%d %H:%M:%S')
         # return datetime.now().isoformat()
         return datetime.now()
 
     def get_score(self):
+        """
+        Get the score of the evaluation, can be 0 (failed), 1 (weak success), 2 (strong success)
+
+        Returns:
+            str: The score of the evaluation
+        """
         return self.score
 
     def get_recommendation(self):
+        """
+        Get the recommandation to give to the user to improve its resource
+
+        Returns:
+            str: The recommandation for the resource tester
+        """
         return self.recommendation
 
     def get_web_resource(self):
+        """
+        Get the WebResource instance of the evaluation previously set
+
+        Returns:
+            WebResource: WebResource object instance
+        """
         return self.web_resource
 
-    # used by FAIRMetrics, will probably be replaced by logs
+    # used by FAIRMetrics, need to be replaced by logs
     def get_reason(self):
         return self.reason
 
     def get_metrics(self):
+        """
+        Get the tag of the metric that evaluate the WebResource
+
+        Returns:
+            str: The tag of the metric evaluating the resource
+        """
         return self.metrics
 
     def get_target_uri(self):
+        """
+        Get the URI/URL of the resource being evaluated
+
+        Returns:
+            str: The URI/URL of the resource
+        """
         return self.target_uri
 
     def get_implem(self):
+        """
+        Get the implementator of the metric, at the moment it is FAIR-Checker, or FAIRMetrics
+
+        Returns:
+            str: The name of the implementator of the metric
+        """
         return self.implem
 
+    def get_test_time(self):
+        """
+        Get the test time delta of the evaluation between start and end
+
+        Returns:
+            datetime: The time lenght of the evaluation
+        """
+        return self.end_time - self.start_time
+
     def persist(self, source="UI"):
+        """
+        Persist in a mongoDB database the resulte of the evaluation, the persisted evaluation has the information if it is executed from the UI or the API
+
+        Args:
+            source (str, optional): _description_. Defaults to "UI".
+
+        Returns:
+            _type_: _description_
+        """
         client = MongoClient()
         db = client.fair_checker
         db_eval = db.evaluations
@@ -170,10 +332,13 @@ class Evaluation:
         r = db_eval.insert_one(eval)
         return r
 
-    def get_test_time(self):
-        return self.end_time - self.start_time
-
     def __str__(self):
+        """
+        The string returner to describe the Evaluation instance
+
+        Returns:
+            str: The content of the description of the Evaluation
+        """
         return (
             f"FAIR metrics evaluation : "
             f"\n\t started at {self.start_time} "
