792cd2bd77984b2ebb683aef46faceb64021e448
Alban Gaignard <alban.gaignard@univ-nantes.fr>
2024-06-12 10:26:12 +0200

    test for alive and working FR instance

---
+++ notebooks/test_fr_instance.py
@@ -0,0 +1,18 @@
+import requests
+import time
+import json
+
+FC_f2a_url = "https://fair-checker.france-bioinformatique.fr/api/check/metric_F2A"
+test_url = "http://bio.tools/bwa"
+
+start = time.time()
+res = requests.get(url=FC_f2a_url, params={"url": test_url})
+eval_in_sec = time.time() - start
+eval = res.json()
+
+print(json.dumps(eval, indent=4))
+
+assert "score" in eval.keys()
+assert "recommendation" in eval.keys()
+
+
