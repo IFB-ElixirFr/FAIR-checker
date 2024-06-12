e77ff3ff99287f12ea882d1a0fa6114ac3b7c904
Alban Gaignard <alban.gaignard@univ-nantes.fr>
2024-06-12 14:18:28 +0200

    code formatting

--- notebooks/test_fr_instance.py
+++ notebooks/test_fr_instance.py
@@ -1,6 +1,7 @@
 import requests
 import time
-import json
+
+# import json
 
 FC_f2a_url = "https://fair-checker.france-bioinformatique.fr/api/check/metric_F2A"
 test_url = "http://bio.tools/bwa"
@@ -10,9 +11,7 @@ res = requests.get(url=FC_f2a_url, params={"url": test_url})
 eval_in_sec = time.time() - start
 eval = res.json()
 
-#print(json.dumps(eval, indent=4))
+# print(json.dumps(eval, indent=4))
 
 assert "score" in eval.keys()
 assert "recommendation" in eval.keys()
-
-
