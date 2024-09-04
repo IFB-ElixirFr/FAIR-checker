48fd4f480944989f6c79cc1b77784e0b77acbcc8
Alban Gaignard <alban.gaignard@univ-nantes.fr>
2024-09-04 11:13:48 +0200

    moved code to util

--- metrics/Evaluation.py
+++ metrics/Evaluation.py
@@ -6,104 +6,12 @@ from io import StringIO
 import uuid
 from string import Template
 
-prefix = """
-@prefix daq: <http://purl.org/eis/vocab/daq#> .
-@prefix dcat: <http://www.w3.org/ns/dcat#> .
-@prefix dcterms: <http://purl.org/dc/terms/> .
-@prefix dqv: <http://www.w3.org/ns/dqv#> .
-@prefix duv: <http://www.w3.org/ns/duv#> .
-@prefix oa: <http://www.w3.org/ns/oa#> .
-@prefix prov: <http://www.w3.org/ns/prov#> .
-@prefix sdmx-attribute: <http://purl.org/linked-data/sdmx/2009/attribute#> .
-@prefix skos: <http://www.w3.org/2004/02/skos/core#> .
-@prefix xsd: <http://www.w3.org/2001/XMLSchema#> .
-@prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .
-
-@prefix : <https://fair-checker.france-bioinformatique.fr/data/> .
-"""
-
-FC_spec = [
-    {"id": "F1A", "category": "Findable", "label": "Unique IDs", "definition": ""},
-    {"id": "F1B", "category": "Findable", "label": "Persistent IDs", "definition": ""},
-    {
-        "id": "F2A",
-        "category": "Findable",
-        "label": "Structured metadata",
-        "definition": "",
-    },
-    {
-        "id": "F2B",
-        "category": "Findable",
-        "label": "Shared vocabularies for metadata",
-        "definition": "",
-    },
-    {
-        "id": "A1.1",
-        "category": "Accessible",
-        "label": "Open resolution protocol",
-        "definition": "",
-    },
-    {
-        "id": "A1.2",
-        "category": "Accessible",
-        "label": "Authorisation procedure or access rights",
-        "definition": "",
-    },
-    {
-        "id": "I1",
-        "category": "Interoperable",
-        "label": "Machine readable format",
-        "definition": "",
-    },
-    {
-        "id": "I2",
-        "category": "Interoperable",
-        "label": "Use shared ontologies",
-        "definition": "",
-    },
-    {
-        "id": "I3",
-        "category": "Interoperable",
-        "label": "External links",
-        "definition": "",
-    },
-    {
-        "id": "R1.1",
-        "category": "Reusable",
-        "label": "Metadata includes license",
-        "definition": "",
-    },
-    {
-        "id": "R1.2",
-        "category": "Reusable",
-        "label": "Metadata includes provenance",
-        "definition": "",
-    },
-    {
-        "id": "R1.3",
-        "category": "Reusable",
-        "label": "Community standards",
-        "definition": "",
-    },
-]
-
-
-FAIR_Checker_template = """
-:$metric_id
-    a dqv:Dimension ;
-    skos:prefLabel "$metric_label"@en ;
-    skos:definition "$metric_definition"@en ;
-    dqv:inCategory :$category ."""
-
-metrics_tpl = """
-:$id
-    a dqv:QualityMeasurement ;
-    dqv:computedOn <$url> ;
-    dqv:isMeasurementOf :$dimension ;
-    dqv:value "$value"^^xsd:integer ;
-    prov:generatedAtTime "$date"^^xsd:dateTime ;
-    prov:wasAttributedTo <https://github.com/IFB-ElixirFr/fair-checker> ;
-    rdfs:seeAlso <https://doi.org/10.1186/s13326-023-00289-5> ."""
+from metrics.util import (
+    ld_FAIR_Checker_template,
+    get_ld_FC_spec,
+    ld_metrics_tpl,
+    ld_eval_prefix,
+)
 
 
 @unique
@@ -308,22 +216,23 @@ class Evaluation:
             d = self.end_time.isoformat()
 
         if self.score:
-            eval_ttl = Template(metrics_tpl).safe_substitute(
+            eval_ttl = Template(ld_metrics_tpl).safe_substitute(
                 id=str(self._id),
                 url=self.get_target_uri().strip(),
                 dimension=self.get_metrics(),
                 value=self.get_score(),
                 date=d,
             )
-        ttl = prefix + eval_ttl
+        ttl = ld_eval_prefix + eval_ttl
 
-        for spec in FC_spec:
+        for spec in get_ld_FC_spec():
             if self.get_metrics() == spec["id"]:
-                spec_ttl = Template(FAIR_Checker_template).safe_substitute(
+                spec_ttl = Template(ld_FAIR_Checker_template).safe_substitute(
                     metric_id=spec["id"],
                     metric_label=spec["label"],
                     metric_definition=spec["definition"],
                     category=spec["category"],
+                    seeAlso=spec["seeAlso"],
                 )
                 ttl += spec_ttl
 
